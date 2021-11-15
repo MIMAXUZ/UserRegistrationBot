dd3fdb491c80b52f339c5bcc586bd4f753ef91b5
O'ktam <oktam.yoqubov@mail.ru>
2021-11-15 14:20:19 +0500

    Fixed Configuration file

--- UserRegistrationBot/Configurations.cs
+++ UserRegistrationBot/Configurations.cs
@@ -4,7 +4,6 @@ namespace UserRegistrationBot
 {
     class Configurations
     {
-        public static string ApiToken { get; private set; } = "1232951908:AAFeoxJ9AcGuDMfZxWo_OyelI6OG6xgzdRU";
-        //static TelegramBotClient regBot = new TelegramBotClient("1232951908:AAFeoxJ9AcGuDMfZxWo_OyelI6OG6xgzdRU");
+        public static string ApiToken { get; private set; } = "<API Token>";
     }
 }
